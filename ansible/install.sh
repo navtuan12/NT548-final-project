28aaace19d02194814465144666b4ac920bc2fed
anhtuan-pc <anhtuan.mmt@gmail.com>
2024-12-26 08:23:14 +0700

    update ansible

--- ansible/install.sh
+++ ansible/install.sh
@@ -7,7 +7,7 @@ python3 -m venv --system-site-packages ansible
 source ansible/bin/activate
 
 sudo apt install ansible-core -y
-export ANSIBLE_HOST_KEY_CHECKING=False
+source ANSIBLE_HOST_KEY_CHECKING=False
 
 ansible-playbook -i inventory.ini 00-init-playbook.yml
 ansible-playbook -i inventory.ini 01-join-node-playbook.yml
